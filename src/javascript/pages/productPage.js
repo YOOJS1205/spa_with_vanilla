dfb2336a59a67237b634b11a6cba8b8d722c1aa1
yoojs1205 <junsang8921@gmail.com>
2022-11-07 16:02:09 +0900

    Feat: 상품 리스트 페이지 구현

--- src/javascript/pages/productPage.js
+++ src/javascript/pages/productPage.js
@@ -1,5 +1,6 @@
 export default class ProductPage {
   constructor() {
+    this.mainElement = document.createElement("main");
     this.product = {};
   }
 
@@ -11,39 +12,66 @@ export default class ProductPage {
     this.product = await data;
   }
 
-  // 상품 리스트 세팅하기
+  // 상품 리스트 세팅하기 (data 뿌려주기)
   async setProductList() {
     await this.getProductData();
     console.log(this.product);
+
+    this.mainElement.classList.add("product");
+    this.mainElement.innerHTML = `
+      <h1 class="ir">상품목록 페이지</h1>
+      <ul class="product-list"></ul>
+    `;
+
+    const productList = this.mainElement.querySelector(".product-list");
+
+    this.product.forEach((item) => {
+      const productDetailLink = document.createElement("a");
+      productDetailLink.href = `/detail/${item.id}`;
+      productDetailLink.innerHTML = `
+        <li class='product-item'>
+          <div class="product-img">
+            <img src='http://test.api.weniv.co.kr/${item.thumbnailImg}' alt='상품이미지' />
+          </div>
+          <strong class="product-name">${item.productName}</strong>
+          <div class="product-price">
+            <strong class="price m-price">${item.price}</strong>   
+          </div>
+        </li>
+      `;
+      productList.appendChild(productDetailLink);
+    });
+
+    console.log(this.mainElement);
   }
 
   render() {
-    const container = document.createElement("div");
-    const element = document.createElement("h1");
-    element.innerText = "상품목록 페이지입니다.";
+    // const container = document.createElement("div");
+    // const element = document.createElement("h1");
+    // element.innerText = "상품목록 페이지입니다.";
 
-    const anchor1 = document.createElement("a");
-    anchor1.href = "/detail/1";
-    anchor1.innerText = "1 상세페이지 이동";
+    // const anchor1 = document.createElement("a");
+    // anchor1.href = "/detail/1";
+    // anchor1.innerText = "1 상세페이지 이동";
 
-    container.appendChild(anchor1);
+    // container.appendChild(anchor1);
 
-    const anchor2 = document.createElement("a");
-    anchor2.href = "/detail/2";
-    anchor2.innerText = "2 상세페이지 이동";
+    // const anchor2 = document.createElement("a");
+    // anchor2.href = "/detail/2";
+    // anchor2.innerText = "2 상세페이지 이동";
 
-    container.appendChild(anchor2);
+    // container.appendChild(anchor2);
 
-    const anchor3 = document.createElement("a");
-    anchor3.href = "/detail/3";
-    anchor3.innerText = "3 상세페이지 이동";
+    // const anchor3 = document.createElement("a");
+    // anchor3.href = "/detail/3";
+    // anchor3.innerText = "3 상세페이지 이동";
 
-    container.appendChild(anchor3);
+    // container.appendChild(anchor3);
 
-    container.appendChild(element);
+    // container.appendChild(element);
 
     this.setProductList();
 
-    return container;
+    return this.mainElement;
   }
 }
